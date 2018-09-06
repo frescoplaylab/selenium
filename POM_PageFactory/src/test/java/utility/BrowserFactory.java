adafcf8cfd6afac47dc87db643a387dc3ac2109c
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2018-09-06 15:48:41 +0530

    Update BrowserFactory.java

--- POM_PageFactory/src/test/java/utility/BrowserFactory.java
+++ POM_PageFactory/src/test/java/utility/BrowserFactory.java
@@ -11,33 +11,17 @@ public class BrowserFactory {
 	
 	public static WebDriver startBrowser(String browserName, String url){
 		
-		if (browserName.equalsIgnoreCase("chrome")){
-		
-			
-			driver.get("https://en.wikipedia.org/wiki/Main_Page");
-			System.setProperty("webdriver.chrome.driver", "/home/vsjanani/Desktop/Selenium_Workspace/Drivers/chromedriver");
-			driver=new ChromeDriver();	
+		if (browserName.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver", "geckodriver local path" );
+			driver= new FirefoxDriver();	
 		}
 		
-		
-		else if (browserName.equalsIgnoreCase("firefox")){
-			
-			
-			System.setProperty("webdriver.gecko.driver", "/home/vsjanani/Desktop/Selenium_Workspace/Drivers/geckodriver" );
-			driver= new FirefoxDriver();
-			
-		}
-		
-		else if (browserName.equalsIgnoreCase("HtmlUnitDriver")){
-			
-			 driver = new HtmlUnitDriver(true);
-			
-			
+		else if (browserName.equalsIgnoreCase("HtmlUnitDriver")){	
+			 driver = new HtmlUnitDriver(true);	
 		}
 		
 		driver.manage().window().maximize();
 		driver.get(url);
-		
 		return driver;
 	}
 }
