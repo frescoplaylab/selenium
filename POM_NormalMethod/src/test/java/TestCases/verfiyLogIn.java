799f68a3c4cbd324b01520f0e8506c69d370c3a9
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2018-09-06 16:27:50 +0530

    Update verfiyLogIn.java

--- POM_NormalMethod/src/test/java/TestCases/verfiyLogIn.java
+++ POM_NormalMethod/src/test/java/TestCases/verfiyLogIn.java
@@ -11,23 +11,23 @@ import Pages.LoginPage;
 public class verfiyLogIn {
 
 	@Test
-	public void VerifyLogin() throws InterruptedException{
-		
-	//	System.setProperty("webdriver.gecko.driver", "/home/vsjanani/Desktop/Selenium_Workspace/Drivers/geckodriver" );
-	//	WebDriver driver = new FirefoxDriver();
-		
+	public void VerifyLogin() throws InterruptedException
+	
 		WebDriver driver = new HtmlUnitDriver(true);
 		driver.get("https://en.wikipedia.org/wiki/Main_Page");
 		Thread.sleep(8000);
 		
-		
+		// Write your code here
+	
 		LoginPage login=new LoginPage(driver);
 		login.clickLoginTab();
 		login.typeUserName();
 		login.typePassword();
 		login.clickLoginButton();
+	
+	
+	
 		System.out.println("Login is successful");
-		
 		
 		driver.quit();
 		System.out.println("Browser Quit is successful");
