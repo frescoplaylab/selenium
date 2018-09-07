d7787eb49403dca7b5bda4d273916064aec5a6ef
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2018-09-07 13:12:24 +0530

    Update verfiyLogIn.java

--- POM_NormalMethod/src/test/java/TestCases/verfiyLogIn.java
+++ POM_NormalMethod/src/test/java/TestCases/verfiyLogIn.java
@@ -10,20 +10,18 @@ public class verfiyLogIn {
 	@Test
 	public void verifyLogin() throws InterruptedException{
 		
-	
 		WebDriver driver = new HtmlUnitDriver(true);
 		driver.get("https://en.wikipedia.org/wiki/Main_Page");
 		Thread.sleep(8000);
 		
+		// Write your code here
 		
-		LoginPage login=new LoginPage(driver);
-		login.clickLoginTab();
-		login.typeUserName();
-		login.typePassword();
-		login.clickLoginButton();
+		
+		
+		
+		
+		
 		System.out.println("Login is successful");
-		
-		
 		driver.quit();
 		System.out.println("Browser Quit is successful");
 	}
