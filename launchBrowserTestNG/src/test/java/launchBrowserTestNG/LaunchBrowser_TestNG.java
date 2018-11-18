25b69dc5399d83fea5fc5296c6642f1fdbb6dc33
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2018-11-18 17:28:27 +0530

    Update LaunchBrowser_TestNG.java

--- launchBrowserTestNG/src/test/java/launchBrowserTestNG/LaunchBrowser_TestNG.java
+++ launchBrowserTestNG/src/test/java/launchBrowserTestNG/LaunchBrowser_TestNG.java
@@ -16,20 +16,12 @@ public class LaunchBrowser_TestNG {
 	  public void LaunchBrowser() throws InterruptedException{
 		
 		
-		// Write your script here
-
-		DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setJavascriptEnabled(true); 
-		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/root/selenium/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
-		WebDriver driver = new PhantomJSDriver(caps);
-		System.out.println("PhantomJS Headless Driver launched");
-		driver.get("https://google.com");
-		Thread.sleep(5000);
-		System.out.println("Launch Browser is successful ");
-		System.out.println("Page Title : " + driver.getTitle());
+				// Write your script here
 
 	
 
+		  
+		  
 				//Searching for "Fresco Play" in Google search
 				// Find the element locators for the below actions.
 		
