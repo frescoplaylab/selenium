06fd0d78df1fcd0ba6ecaa5a3d4b519e3c060a59
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2019-01-25 14:21:33 +0530

    Add files via upload

--- launchBrowserTestNG/src/test/java/launchBrowserTestNG/LaunchBrowser_TestNG.java
+++ launchBrowserTestNG/src/test/java/launchBrowserTestNG/LaunchBrowser_TestNG.java
@@ -14,32 +14,36 @@ public class LaunchBrowser_TestNG {
 	@Test
 	  public void LaunchBrowser() throws InterruptedException{
 		
-				// Write your script here
+		
+		// Write your script here
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setJavascriptEnabled(true); 
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/root/selenium/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
+		WebDriver driver = new PhantomJSDriver(caps);
+		System.out.println("PhantomJS Headless Driver launched");
+		
+		driver.get("https://google.com");
+		Thread.sleep(5000);
+		System.out.println("Launch Browser is successful ");
+		System.out.println("Page Title : " + driver.getTitle());
 
 	
 
-		  
-		  
-		  
-		  
-				//Searching for "Fresco Play" in Google search
-				// Find the element locators for the below actions.
-	
-				// 1) Find locator for Google Search Box and sendkeys as Fresco Play as below.
-				driver.findElement(By.("")).sendKeys("Fresco Play");
-				
-				// 2) Find locator for Google Search Box and sendkeys as Enter  as below.
-				driver.findElement(By.("")).sendKeys(Keys.ENTER);
-				Thread.sleep(6000);
-				System.out.println("Page Title : " + driver.getTitle());
-						
-						
-				//Click "Fresco Apps" link from search list
-				// 3) Find locator for Google Search Results and click  Fresco Apps link.
-				Thread.sleep(5000);
-				driver.findElement(By.("")).click();
-				Thread.sleep(5000);
-				System.out.println("Page Title : " + driver.getTitle());
+		//Searching for "Fresco Play" in Google search
+			
+		driver.findElement(By.("")).sendKeys("Fresco Play");
+		driver.findElement(By.("")).sendKeys(Keys.ENTER);
+		Thread.sleep(5000);
+		System.out.println("Page Title : " + driver.getTitle());
+					
+					
+		//Click "Fresco Apps" link from search list
+			
+		driver.findElement(By.("")).click();
+		Thread.sleep(5000);
+		System.out.println("Page Title : " + driver.getTitle());
+		driver.quit();
 	}
 }
 
