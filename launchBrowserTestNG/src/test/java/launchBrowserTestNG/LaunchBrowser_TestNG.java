53d875838046a755ca4e713daea6daf7325b0c74
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2019-01-25 13:40:52 +0530

    Update LaunchBrowser_TestNG.java

--- launchBrowserTestNG/src/test/java/launchBrowserTestNG/LaunchBrowser_TestNG.java
+++ launchBrowserTestNG/src/test/java/launchBrowserTestNG/LaunchBrowser_TestNG.java
@@ -3,7 +3,6 @@ package launchBrowserTestNG;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,6 +14,23 @@ public class LaunchBrowser_TestNG {
 	@Test
 	  public void LaunchBrowser() throws InterruptedException{
 		
+	
+		
+		
+		// Write your script here
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setJavascriptEnabled(true); 
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/root/selenium/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
+		WebDriver driver = new PhantomJSDriver(caps);
+		System.out.println("PhantomJS Headless Driver launched");
+		
+		driver.get("https://google.com");
+		Thread.sleep(5000);
+		System.out.println("Launch Browser is successful ");
+		System.out.println("Page Title : " + driver.getTitle());
+
+	
 		
 				// Write your script here
 
