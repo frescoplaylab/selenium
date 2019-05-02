e047d684586ab12db9849523fd5093ebb4444992
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2019-05-02 13:29:21 +0530

    Update googleLaunch.java

--- CrossBrowsers/src/test/java/crossBrowsers/googleLaunch.java
+++ CrossBrowsers/src/test/java/crossBrowsers/googleLaunch.java
@@ -26,23 +26,10 @@ public class googleLaunch {
 	{
 	
 		// Write your script here  
-		if (browser.equalsIgnoreCase("ChromeHeadless"))
-		{
-			ChromeDriverManager.chromedriver().setup();
-			ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.addArguments("--headless");
-			driver = new ChromeDriver(chromeOptions);
-			System.out.println("Chrome Headless Driver launched");
-		}
 		
-		else if (browser.equalsIgnoreCase("PhantomJS"))
-		{
-			 DesiredCapabilities caps = new DesiredCapabilities();
-			 caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
-			 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/root/selenium/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
-			 driver = new PhantomJSDriver(caps);
-			System.out.println("PhantomJS Headless Driver launched");
-		}
+		
+		
+		
 	
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
@@ -54,17 +41,17 @@ public class googleLaunch {
 		// Searching for "Fresco Play" in Google search.
 		// Find the elementlocators for the below actions.
 		// 1) Find locator for Google Search Box and sendkeys as Fresco Play as below.
-		driver.findElement(By.name("q")).sendKeys("Fresco Play");
+		driver.findElement(By.("")).sendKeys("Fresco Play");
 		
 		// 2) Find locator for Google Search Box and sendkeys as Enter  as below.
-		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		driver.findElement(By.("")).sendKeys(Keys.ENTER);
 		Thread.sleep(6000);
 		System.out.println("Page Title : " + driver.getTitle());
 					
 		// Click "Fresco Apps" link from search list.
 		// 3) Find locator for Google Search Results and click  Fresco Apps link.
 		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@class='LC20lb']")).click();
+		driver.findElement(By.("")).click();
 		Thread.sleep(5000);
 		System.out.println("Page Title : " + driver.getTitle());
 		
